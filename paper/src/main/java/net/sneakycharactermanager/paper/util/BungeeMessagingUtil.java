b3fbebb62ef3839770683a9d1d9900c26db91bc6
xGinko <rachel.audrey@mail.de>
2024-05-16 22:04:07 +0200

    beautiful recursion

--- paper/src/main/java/net/sneakycharactermanager/paper/util/BungeeMessagingUtil.java
+++ paper/src/main/java/net/sneakycharactermanager/paper/util/BungeeMessagingUtil.java
@@ -4,64 +4,47 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 import org.bukkit.entity.Player;
 
 import net.sneakycharactermanager.paper.SneakyCharacterManager;
+import org.bukkit.plugin.messaging.PluginMessageRecipient;
 
 public class BungeeMessagingUtil {
 
     public static void sendByteArray(Player requester, String subChannelName, Object... objects) {
+        SneakyCharacterManager plugin = SneakyCharacterManager.getInstance();
         try (ByteArrayOutputStream byteArrayOutput = new ByteArrayOutputStream();
             DataOutputStream out = new DataOutputStream(byteArrayOutput)) {
 
             out.writeUTF(subChannelName + "_UUID:" + UUID.randomUUID());
 
             for (Object object : objects) {
-                if (object.getClass() == Boolean.class)
-                    out.writeBoolean((boolean) object);
-                else if (object.getClass() == Byte.class || object.getClass() == byte.class)
-                    out.writeByte((int) object);
-                else if (object.getClass() == Double.class || object.getClass() == double.class)
-                    out.writeDouble((double) object);
-                else if (object.getClass() == Float.class || object.getClass() == float.class)
-                    out.writeFloat((float) object);
-                else if (object.getClass() == Integer.class || object.getClass() == int.class)
-                    out.writeInt((int) object);
-                else if (object.getClass() == Long.class || object.getClass() == long.class)
-                    out.writeLong((long) object);
-                else if (object.getClass() == Short.class || object.getClass() == short.class)
-                    out.writeShort((int) object);
-                else if (object.getClass() == String.class)
-                    out.writeUTF((String) object);
-                else if (object instanceof List && ((List<?>) object).isEmpty())
-                    out.writeInt(0);
-                else if (object instanceof List && ((List<?>) object).get(0) instanceof String)
-                    writeStringList(out, (List<String>) object);
-                else {
-                    SneakyCharacterManager.getInstance().getLogger().severe("SneakyCharacterManager attempted to write an unidentified object to a ByteArray!");
-                    return;
-                }
+                writeToOutputStream(out, object);
             }
 
-
-            if(requester == null){
-                SneakyCharacterManager.getInstance().getServer().sendPluginMessage(SneakyCharacterManager.getInstance(), "sneakymouse:" + SneakyCharacterManager.IDENTIFIER, byteArrayOutput.toByteArray());
-            }else {
-                requester.sendPluginMessage(SneakyCharacterManager.getInstance(), "sneakymouse:" + SneakyCharacterManager.IDENTIFIER, byteArrayOutput.toByteArray());
-            }
-
+            PluginMessageRecipient recipient = requester == null ? plugin.getServer() : requester;
+            recipient.sendPluginMessage(plugin, "sneakymouse:" + SneakyCharacterManager.IDENTIFIER, byteArrayOutput.toByteArray());
         } catch (IOException e) {
-            e.printStackTrace();
+            plugin.getServer().getLogger().throwing("BungeeMessagingUtil", "sendByteArray", e);
         }
     }
 
-    private static void writeStringList(DataOutputStream out, List<String> strings) throws IOException {
-        out.writeInt(strings.size());
-        for (String string : strings) {
-            out.writeUTF(string);
+    private static void writeToOutputStream(DataOutputStream out, Object object) throws IOException {
+        if (object instanceof Boolean bool) out.writeBoolean(bool);
+        else if (object instanceof Byte b) out.writeByte(b);
+        else if (object instanceof Double d) out.writeDouble(d);
+        else if (object instanceof Float f) out.writeFloat(f);
+        else if (object instanceof Integer i) out.writeInt(i);
+        else if (object instanceof Long l) out.writeLong(l);
+        else if (object instanceof Short s) out.writeShort(s);
+        else if (object instanceof String str) out.writeUTF(str);
+        else if (object instanceof List<?> list) {
+            out.writeInt(list.size());
+            for (Object listObject : list) writeToOutputStream(out, listObject);
         }
+        else throw new IOException("Cannot write unidentified Object to ByteArray.");
     }
-
 }
